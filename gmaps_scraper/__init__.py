b36a111f0354a20a1bdf534ae380d518becf7916
Matias Iglesias <matuteiglesias@gmail.com>
2026-09-07 16:58:38 -0300

    build: align runtime version with unreleased package metadata

--- gmaps_scraper/__init__.py
+++ gmaps_scraper/__init__.py
@@ -1,7 +1,7 @@
 from .kernel import BusinessRecord, BusinessRef
 from .providers import DiscoveryRequest, GeoCircle, ProviderSpec, get_provider, provider_names
 
-__version__ = "0.2.0"
+__version__ = "0.2.0.dev0"
 
 __all__ = [
     "BusinessRecord",
