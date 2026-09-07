5839005fabe4a7b93dd95eca5e2370b4ddf78bbe
Matias Iglesias <matuteiglesias@gmail.com>
2026-09-07 16:46:17 -0300

    build: expose package version and kernel contracts

---
+++ gmaps_scraper/__init__.py
@@ -0,0 +1,14 @@
+from .kernel import BusinessRecord, BusinessRef
+from .providers import DiscoveryRequest, ProviderSpec, get_provider, provider_names
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "BusinessRecord",
+    "BusinessRef",
+    "DiscoveryRequest",
+    "ProviderSpec",
+    "get_provider",
+    "provider_names",
+    "__version__",
+]
